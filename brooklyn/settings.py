1f77248d14565a759ba51334783fd0ab8bc677b1
Yurii Roik <roikyuriy@gmail.com>
2017-05-29 23:12:48 +0300

    allowed host + debug false

--- brooklyn/settings.py
+++ brooklyn/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'll6mmb&#vh)w5ch%6$h18p_6jw(rfd5eicpnziw-vhrkyfa+2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 	'192.168.0.76',
 	'roikyuriy.pythonanywhere.com',
 	'127.0.0.1'
+	'brooklyn99.pythonanywhere.com',
 ]
 
 
